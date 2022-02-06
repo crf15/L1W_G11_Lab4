7119a415f542a742dacdec494ff17e84eb2155c2
Kenny Deng <kennyddeng@gmail.com>
2022-02-06 12:39:45 -0500

    Delete add_authorized_users.py

--- add_authorized_users.py
+++
@@ -1,6 +0,0 @@
-from backend import Backend
-from mydbconfig import *
-backend = Backend(config, email, firstname, lastname)
-
-backend.add_authorized_users('cristianfigueroa@cmail.carleton.ca')
-backend.add_authorized_users('my_group_mate2_email@cmail.carleton.ca')
