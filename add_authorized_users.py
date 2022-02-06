1eac1d6367adb737e2a09ed8efe654c0df08aaac
Kenny Deng <kennydeng@cmail.carleton.ca>
2022-02-06 12:19:09 -0500

    feat: Added capability to clear leds

--- add_authorized_users.py
+++ add_authorized_users.py
@@ -2,5 +2,5 @@ from backend import Backend
 from mydbconfig import *
 backend = Backend(config, email, firstname, lastname)
 
-backend.add_authorized_users('my_group_mate1_email@cmail.carleton.ca')
+backend.add_authorized_users('cristianfigueroa@cmail.carleton.ca')
 backend.add_authorized_users('my_group_mate2_email@cmail.carleton.ca')
